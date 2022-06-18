2b6d4a0196bffd38fc9fc492ff1920336cd3f358
s3r0s4pi3ns <s3r0s4pi3ns@protonmail.com>
2022-06-18 11:39:06 +0100

    Initial setup with basic configuration, linter, prettier, security headers ...

---
+++ next.config.js
@@ -0,0 +1,82 @@
+/** @type {import('next').NextConfig} */
+
+// https://securityheaders.com
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com;
+  child-src *.youtube.com *.google.com;
+  style-src 'self' 'unsafe-inline' *.googleapis.com;
+  img-src * blob: data:;
+  media-src 'none';
+  connect-src *;
+  font-src 'self';
+`;
+
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: [],
+  },
+  // i18n: {
+  //   locales: ['es', 'en'],
+  //   defaultLocale: 'en',
+  // },
+  optimizeFonts: true,
+  poweredByHeader: false,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true,
+      },
+    ];
+  },
+  //https://nextjs.org/docs/advanced-features/security-headers
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\n/g, ''),
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'Server',
+            value: 'Apache', // phony server value
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value:
+              'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+          },
+        ],
+      },
+    ];
+  },
+};
